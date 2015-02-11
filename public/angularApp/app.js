267347512e5b63e7c3c7885fd6afe0f3014d551b
penguinsource <mihaioprescu5@gmail.com>
2015-02-11 00:51:53 -0700

    done checking for mills on Placement mode

--- public/angularApp/app.js
+++ public/angularApp/app.js
@@ -110,34 +110,36 @@ nineApp.controller('basicController', function($scope, $http){
 
 	// sample position object: { "player": "none", "neighbours": [ [0,3], [] }
 	// self.p0 = "player1";
-	self.p0 = {"control": "free", "neighbours": [ 1, 7 ], "pclass": "pinFreePlace" };
-	self.p1 = {"control": "free", "neighbours": [ 0, 2, 9 ], "pclass": "pinFreePlace" };
-	self.p2 = {"control": "free", "neighbours": [ 1, 3 ], "pclass": "pinFreePlace" };
-	self.p3 = {"control": "free", "neighbours": [ 2, 4, 11 ], "pclass": "pinFreePlace" };
-	self.p4 = {"control": "free", "neighbours": [ 3, 5 ], "pclass": "pinFreePlace" };
-	self.p5 = {"control": "free", "neighbours": [ 4, 6, 13 ], "pclass": "pinFreePlace" };
-	self.p6 = {"control": "free", "neighbours": [ 5, 7 ], "pclass": "pinFreePlace" };
-	self.p7 = {"control": "free", "neighbours": [ 0, 6, 15 ], "pclass": "pinFreePlace" };
-	self.p8 = {"control": "free", "neighbours": [ 9, 15 ], "pclass": "pinFreePlace" };
-	self.p9 = {"control": "free", "neighbours": [ 1, 8, 10, 17 ], "pclass": "pinFreePlace" };
-	self.p10 = {"control": "free", "neighbours": [ 9, 11 ], "pclass": "pinFreePlace" };
-	self.p11 = {"control": "free", "neighbours": [ 3, 10, 12, 19 ], "pclass": "pinFreePlace" };
-	self.p12 = {"control": "free", "neighbours": [ 11, 13 ], "pclass": "pinFreePlace" };
-	self.p13 = {"control": "free", "neighbours": [ 5, 12, 14, 21 ], "pclass": "pinFreePlace" };
-	self.p14 = {"control": "free", "neighbours": [ 13, 15 ], "pclass": "pinFreePlace" };
-	self.p15 = {"control": "free", "neighbours": [ 7, 8, 14, 23 ], "pclass": "pinFreePlace" };
-	self.p16 = {"control": "free", "neighbours": [ 17, 23 ], "pclass": "pinFreePlace" };
-	self.p17 = {"control": "free", "neighbours": [ 9, 16, 18 ], "pclass": "pinFreePlace" };
-	self.p18 = {"control": "free", "neighbours": [ 17, 19 ], "pclass": "pinFreePlace" };
-	self.p19 = {"control": "free", "neighbours": [ 11, 18, 20 ], "pclass": "pinFreePlace" };
-	self.p20 = {"control": "free", "neighbours": [ 19, 21 ], "pclass": "pinFreePlace" };
-	self.p21 = {"control": "free", "neighbours": [ 13, 20, 22 ], "pclass": "pinFreePlace" };
-	self.p22 = {"control": "free", "neighbours": [ 21, 23 ], "pclass": "pinFreePlace" };
-	self.p23 = {"control": "free", "neighbours": [ 15, 16, 22 ], "pclass": "pinFreePlace" };
+	self.p0 = {"control": "free", "vNeighbours": [ 7 ], "hNeighbours": [ 1 ], "pclass": "pinFreePlace" };
+	self.p1 = {"control": "free", "vNeighbours": [ 9 ], "hNeighbours": [ 0, 2 ], "pclass": "pinFreePlace" };
+	self.p2 = {"control": "free", "vNeighbours": [ 3 ], "hNeighbours": [ 1 ], "pclass": "pinFreePlace" };
+	self.p3 = {"control": "free", "vNeighbours": [ 2, 4 ], "hNeighbours": [ 11 ], "pclass": "pinFreePlace" };
+	self.p4 = {"control": "free", "vNeighbours": [ 3 ], "hNeighbours": [ 5 ], "pclass": "pinFreePlace" };
+	self.p5 = {"control": "free", "vNeighbours": [ 13 ], "hNeighbours": [ 4, 6 ], "pclass": "pinFreePlace" };
+	self.p6 = {"control": "free", "vNeighbours": [ 7 ], "hNeighbours": [ 5 ], "pclass": "pinFreePlace" };
+	self.p7 = {"control": "free", "vNeighbours": [ 0, 6 ], "hNeighbours": [ 15 ], "pclass": "pinFreePlace" };
+	self.p8 = {"control": "free", "vNeighbours": [ 15 ], "hNeighbours": [ 9 ], "pclass": "pinFreePlace" };
+	self.p9 = {"control": "free", "vNeighbours": [ 1, 17 ], "hNeighbours": [ 8, 10 ], "pclass": "pinFreePlace" };
+	self.p10 = {"control": "free", "vNeighbours": [ 11 ], "hNeighbours": [ 9 ], "pclass": "pinFreePlace" };
+	self.p11 = {"control": "free", "vNeighbours": [ 10, 12 ], "hNeighbours": [ 3, 19 ], "pclass": "pinFreePlace" };
+	self.p12 = {"control": "free", "vNeighbours": [ 11 ], "hNeighbours": [ 13 ], "pclass": "pinFreePlace" };
+	self.p13 = {"control": "free", "vNeighbours": [ 5, 21 ], "hNeighbours": [ 12, 14 ], "pclass": "pinFreePlace" };
+	self.p14 = {"control": "free", "vNeighbours": [ 15 ], "hNeighbours": [ 13 ], "pclass": "pinFreePlace" };
+	self.p15 = {"control": "free", "vNeighbours": [ 8, 14 ], "hNeighbours": [ 7, 23 ], "pclass": "pinFreePlace" };
+	self.p16 = {"control": "free", "vNeighbours": [ 23 ], "hNeighbours": [ 17 ], "pclass": "pinFreePlace" };
+	self.p17 = {"control": "free", "vNeighbours": [ 9 ], "hNeighbours": [ 16, 18 ], "pclass": "pinFreePlace" };
+	self.p18 = {"control": "free", "vNeighbours": [ 19 ], "hNeighbours": [ 17 ], "pclass": "pinFreePlace" };
+	self.p19 = {"control": "free", "vNeighbours": [ 18, 20 ], "hNeighbours": [ 11 ], "pclass": "pinFreePlace" };
+	self.p20 = {"control": "free", "vNeighbours": [ 19 ], "hNeighbours": [ 21 ], "pclass": "pinFreePlace" };
+	self.p21 = {"control": "free", "vNeighbours": [ 13 ], "hNeighbours": [ 20, 22 ], "pclass": "pinFreePlace" };
+	self.p22 = {"control": "free", "vNeighbours": [ 23 ], "hNeighbours": [ 21 ], "pclass": "pinFreePlace" };
+	self.p23 = {"control": "free", "vNeighbours": [ 16, 22 ], "hNeighbours": [ 15 ], "pclass": "pinFreePlace" };
 
 	self.pinList = [];
 	for (var i = 0; i < 24; i++){
-		self.pinList.push(eval("self.p" + i));
+		var obj = eval("self.p" + i);
+		obj.ind = i;
+		self.pinList.push(obj);
 	}
 	
 	console.log("pinlist:");
@@ -152,8 +154,44 @@ nineApp.controller('basicController', function($scope, $http){
 	self.checkForMills = function(pinBtn, position){
 		if (self.isEven(position)){
 			console.log("its even !");
+			var middlePin = eval("self.p" + pinBtn.hNeighbours);
+			var otherPin = {};
+			for (var i = 0; i < middlePin.hNeighbours.length; i++){
+				if (middlePin.hNeighbours[i] != pinBtn.ind){
+					otherPin = eval("self.p" + middlePin.hNeighbours[i]);
+				}
+			}
+			console.log("my pin index is: " + pinBtn.ind);
+			console.log("middle pin index is: " + middlePin.ind);
+			console.log("other pin index is: " + otherPin.ind);
 		} else {
 			console.log("its odd !");
+			
+			// check for horizontal mill
+			var horizontalMill = true;
+			for (var i = 0; i < pinBtn.hNeighbours.length; i++){
+				if (pinBtn.hNeighbours[i].control != self.playerColor){
+					horizontalMill = false;
+					break;
+				}
+			}
+			if (horizontalMill){
+				// highlight mill
+				break;
+			}
+			
+			// check for vertical mill
+			var verticalMill = true;
+			for (var i = 0; i < pinBtn.hNeighbours.length; i++){
+				if (pinBtn.hNeighbours[i].control != self.playerColor){
+					verticalMill = false;
+					break;
+				}
+			}
+			if (verticalMill){
+				// highlight mill
+				break;
+			}
 		}
 	}
 
