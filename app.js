8c9f25c9d6337a4a0c718ee8a2ad61087758dc50
Mihai Oprescu <mihaioprescu5@gmail.com>
2015-04-12 02:08:12 -0600

    game queue, more progress - not finished

--- app.js
+++ app.js
@@ -44,15 +44,43 @@ self.game_queue = [];
 self.active_users["billy"] = { "socketId": null, "gamesWon": 0, "gamesLost": 0, "currentGameId": "fdf4325" };
 self.active_games["fdf4325"] = {};
 
-self.addPlayerToQueue = function(data){
+// create object for user with id 'userid' if one doesn't exist already
+self.addUserToActiveUsers = function(userid, socketid){
+	console.log("add user to active users");
+	console.log(self.active_users[userid]);
+
+	if ( (self.active_users[userid] === undefined) || 
+		 (self.active_users[userid] === null) ){
+		
+		self.active_users[userid] = { "socketId": socketid, 
+									  "gamesWon": 0,
+									  "gamesLost": 0,
+									  "currentGameId": null };
+	} else {
+		// if user with 'userid' is already defined, then update their socketid
+		console.log("Updating socket id. Old socketid: " + 
+			self.active_users[userid].socketId + " to " + socketid );
+		
+		self.active_users[userid].socketId = socketid;
+	}
+
+	console.log("add user to active users");
+	console.log(self.active_users[userid]);
+}
+
+self.addPlayerToQueue = function(data, socket){
 	console.log("Adding player to queue !");
 	console.log(data);
+
+	console.log("SOCKET IDDDDD: ");
+	console.log(socket.id);
 
 	console.log("Game Queue:");
 	console.log(self.game_queue);
 	console.log(self.active_users['hahaaa']);
 	// self.game_queue.push('hello');
-	// self.game_queue.push('car');
+
+	self.addUserToActiveUsers(data.userid, socket.id);
 
 	// check if the player is already in the game queue
 	if (self.game_queue.indexOf(data.userid) === -1){
@@ -62,22 +90,20 @@ self.addPlayerToQueue = function(data){
 	// match players
 	if (self.game_queue.length > 1){
 		// remove the first 2 players from the queue
-
+		var p1 = self.game_queue.pop();
+		var p2 = self.game_queue.pop();
+		console.log("p1: " + p1);
+		console.log("p2: " + p2);
 		// send each a message
 	}
 }
-
-// this.dude = 5;
-
-// console.log("=-========== " + this.dude);
-// console.log("=-========== " + self.dude);
 
 function handleSocketRequests(io){
 	io.on('connection', function (socket) {
 		console.log("your socket id is: " + socket.id);
 
-		socket.on('addPlayerToQueue', self.addPlayerToQueue );
-
+		// socket.on('addPlayerToQueue', self.addPlayerToQueue );
+		socket.on('addPlayerToQueue', function(data){ self.addPlayerToQueue(data, socket); });
 
 		// console.log(socket);
 
